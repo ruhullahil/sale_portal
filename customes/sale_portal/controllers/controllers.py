5cfc57b94cf6c78807893b9f47ce9243f3ad5d53
ruhullahil <ruhullahilt@gmail.com>
2025-01-04 19:09:10 +0600

    [ADD]

--- customes/sale_portal/controllers/controllers.py
+++ customes/sale_portal/controllers/controllers.py
@@ -2,14 +2,74 @@ from odoo import http
 from odoo.http import request
 from odoo import conf, http, _
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+import uuid
+
+
+from odoo.addons.project.controllers.project_sharing_chatter import ProjectSharingChatter
+
+class ChatterPortal(ProjectSharingChatter):
+
+    def _generate_access_token(self):
+        return str(uuid.uuid4())
+
+    def _check_sale_access_and_get_token(self, res_model, res_id, token=None):
+        sale = request.env[res_model].sudo().with_context(active_test=False).search([('id', '=', res_id)])
+        sale[sale._mail_post_token_field] = self._generate_access_token()
+
+        return sale[sale._mail_post_token_field]
+
+
+    @http.route()
+    def portal_message_fetch(self, res_model, res_id, domain=False, limit=10, offset=0, **kw):
+        if res_model == 'sale.order':
+            token = self._check_sale_access_and_get_token(res_model, res_id, kw.get('token'))
+            if token is not None:
+                kw['token'] = token  # Update token (either string which contains token value or False)
+        return super().portal_message_fetch(res_model, res_id, domain=domain, limit=limit, offset=offset, **kw)
+
+    @http.route()
+    def portal_chatter_init(self, res_model, res_id, domain=False, limit=False, **kwargs):
+        is_user_public = request.env.user.has_group('base.group_public')
+        message_data = self.portal_message_fetch(res_model, res_id, domain=domain, limit=limit, **kwargs)
+        display_composer = False
+        if res_model == 'sale.order':
+            token = self._check_sale_access_and_get_token(res_model, res_id, kwargs.get('token'))
+            if token is not None:
+                kwargs['token'] = token
+        if kwargs.get('allow_composer'):
+            display_composer = kwargs.get('token') or not is_user_public
+        return {
+            'messages': message_data['messages'],
+            'options': {
+                'message_count': message_data['message_count'],
+                'is_user_public': is_user_public,
+                'is_user_employee': request.env.user._is_internal(),
+                'is_user_publisher': request.env.user.has_group('website.group_website_restricted_editor'),
+                'display_composer': display_composer,
+                'partner_id': request.env.user.partner_id.id
+            }
+        }
+
 
 
 class SalePortal(CustomerPortal):
 
-    def _check_sale_access_and_get_token(self, project_id, res_model, res_id, token):
-        sale = request.env[res_model].sudo().with_context(active_test=False).search([('id', '=', res_id)])
+    def _prepare_quotations_domain(self, partner):
+        return ['|',
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                ('creator_user','=',request.env.user.id),
+            ('state', '=', 'sent')
+        ]
 
-        return sale[sale._mail_post_token_field]
+    def _prepare_orders_domain(self, partner):
+        return [
+            '|',
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+            ('creator_user', '=', request.env.user.id),
+            ('state', '=', 'sale'),
+        ]
+
+
 
 
 
@@ -61,13 +121,7 @@ class SalePortal(CustomerPortal):
             {'session_info': self._prepare_sales_sharing_session_info()},
         )
 
-    @http.route()
-    def portal_message_fetch(self, res_model, res_id, domain=False, limit=10, offset=0, **kw):
-        if res_model == 'sale.order':
-            token = self._check_sale_access_and_get_token(res_model, res_id, kw.get('token'))
-            if token is not None:
-                kw['token'] = token  # Update token (either string which contains token value or False)
-        return super().portal_message_fetch(res_model, res_id, domain=domain, limit=limit, offset=offset, **kw)
+
 
     # @http.route('/sale_portal/sale_portal/objects', auth='public')
     # def list(self, **kw):
--- customes/sale_portal/models/models.py
+++ customes/sale_portal/models/models.py
@@ -5,7 +5,20 @@ class SaleOrder(models.Model):
 
 
     from_portal = fields.Boolean(default=False)
-    creator_user = fields.Many2one('res.users',delault=lambda self:self.env.user.id)
+    creator_user = fields.Many2one('res.users',compute='_compute_user',store=True)
+    message_follower_ids = fields.One2many(
+        'mail.followers', 'res_id', string='Followers', groups='base.group_user,base.group_portal')
+
+    @api.depends('partner_id')
+    def _compute_user(self):
+        for rec in self:
+            rec.creator_user = self.env.user
+
+
+
+
+
+
 
 
 
